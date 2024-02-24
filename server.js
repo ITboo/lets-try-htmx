aa70a5406493a36f3a3baad4a68a661dc666316b
ITboo <annaacdc@tut.by>
2024-02-24 14:23:14 +0300

    init: add simple express server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+})
