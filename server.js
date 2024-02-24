312aeaadc59cdf7cd882d304009fd0fca04c70e7
ITboo <annaacdc@tut.by>
2024-02-24 16:54:16 +0300

    feat: get data from API

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/users', async (req, res) => {
+
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+    );
+    const users = await response.json();
+
+    res.send(`
+      <h1 class="text-2xl font-bold my-4">Users</h1>
+      <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join('')}
+      </ul>
+    `);
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
 })
